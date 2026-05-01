d3ea616fc5b4aba5119b0d80e05ca6894d04055a
Maxwell Luong <maxwell.luong@allthingsengineering.io>
2026-05-01 15:28:28 -0700

    auto: save changes

--- source/usr/local/emhttp/plugins/unraid-stats2mqtt/scripts/publishers/cli/system.sh
+++ source/usr/local/emhttp/plugins/unraid-stats2mqtt/scripts/publishers/cli/system.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-# SOURCE: /proc/uptime
-# Fields: system uptime in seconds
 
 publish_uptime() {
   local expire="${1:-0}" retain="${2:-true}"
-  local uptime_seconds
-  uptime_seconds=$(awk '{printf "%d", $1}' /proc/uptime 2>/dev/null)
+  local uptime_seconds; uptime_seconds=$(collect_uptime)
   [ -z "$uptime_seconds" ] && return
   local base="${MQTT_BASE_TOPIC}/sensor/${MQTT_TOPIC}"
   ha_register "system_uptime" "System Uptime" "${base}_system_uptime/state" "s" "duration" "timer-outline" "" "$expire"
