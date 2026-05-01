13d5cb13c7683cbd3f033ee51983c971ca013a8a
Maxwell Luong <maxwell.luong@allthingsengineering.io>
2026-05-01 14:43:00 -0700

    auto: save changes

--- source/usr/local/emhttp/plugins/unraid-stats2mqtt/scripts/lib/config.sh
+++ source/usr/local/emhttp/plugins/unraid-stats2mqtt/scripts/lib/config.sh
@@ -3,7 +3,7 @@
 load_config() {
   [ -f "$CONFIG_FILE" ] && source "$CONFIG_FILE"
   UNRAID_API_KEY="${UNRAID_API_KEY:-}"
-  UNRAID_API_HOST="${UNRAID_API_HOST:-https://localhost}"
+  UNRAID_API_HOST="${UNRAID_API_HOST:-http://localhost}"
   MQTT_HOST="${MQTT_HOST:-localhost}"
   MQTT_PORT="${MQTT_PORT:-1883}"
   MQTT_PROTOCOL="${MQTT_PROTOCOL:-mqtt}"
