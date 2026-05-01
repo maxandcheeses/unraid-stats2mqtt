5f9f91e44793d89778ec4628f0e221bd83dedfd1
Maxwell Luong <maxwell.luong@allthingsengineering.io>
2026-05-01 14:52:25 -0700

    auto: save changes

--- source/usr/local/emhttp/plugins/unraid-stats2mqtt/scripts/lib/helpers.sh
+++ source/usr/local/emhttp/plugins/unraid-stats2mqtt/scripts/lib/helpers.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-safe_name()  { echo "$1" | tr ' ' '_' | tr -dc '[:alnum:]_'; }
-is_enabled() { [[ "$1" == "onchange" || "$1" == "interval" || "$1" == "both" ]]; }
+safe_name() { echo "$1" | tr ' ' '_' | tr -dc '[:alnum:]_'; }
 
 json_escape() {
   local s="$1"
@@ -11,19 +10,4 @@ json_escape() {
   s="${s//$'\r'/\\r}"
   s="${s//$'\t'/\\t}"
   echo "$s"
-}
-
-read_ini_section() {
-  local file="$1" section="$2"
-  [ ! -f "$file" ] && return
-  awk -v sec="$section" '
-    $0 == "["sec"]" { in_sec=1; next }
-    /^\[/            { in_sec=0 }
-    in_sec && /=/    { print }
-  ' "$file" 2>/dev/null
-}
-
-ini_field() {
-  local data="$1" key="$2"
-  echo "$data" | grep -E "^${key}=" | head -1 | cut -d= -f2- | tr -d '"'
 }
