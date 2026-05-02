7f028e1639cd9805899460fda70ab909003b3d5d
Maxwell Luong <maxwell.luong@allthingsengineering.io>
2026-05-01 19:32:53 -0700

    auto: save changes

--- source/usr/local/emhttp/plugins/unraid-stats2mqtt/scripts/publishers/unraid-api/system.sh
+++ source/usr/local/emhttp/plugins/unraid-stats2mqtt/scripts/publishers/unraid-api/system.sh
@@ -3,8 +3,16 @@
 publish_uptime() {
   local expire="${1:-0}" retain="${2:-true}"
   local info; info=$(get_info_data) || return
-  local uptime_seconds; uptime_seconds=$(echo "$info" | jq -r '.data.info.os.uptime // empty')
-  [ -z "$uptime_seconds" ] && return
+  local uptime_raw; uptime_raw=$(echo "$info" | jq -r '.data.info.os.uptime // empty')
+  [ -z "$uptime_raw" ] && return
+  local uptime_seconds boot_epoch now_epoch
+  if [[ "$uptime_raw" =~ ^[0-9]+$ ]]; then
+    uptime_seconds="$uptime_raw"
+  else
+    boot_epoch=$(date -d "$uptime_raw" +%s 2>/dev/null) || return
+    now_epoch=$(date +%s)
+    uptime_seconds=$(( now_epoch - boot_epoch ))
+  fi
   local days=$(( uptime_seconds / 86400 ))
   local hours=$(( (uptime_seconds % 86400) / 3600 ))
   local minutes=$(( (uptime_seconds % 3600) / 60 ))
