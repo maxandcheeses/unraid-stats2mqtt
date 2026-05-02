002ae2f6de1eb60d126e67cd45a14c974682f5ff
Maxwell Luong <maxwell.luong@allthingsengineering.io>
2026-05-01 19:31:16 -0700

    auto: save changes

--- source/usr/local/emhttp/plugins/unraid-stats2mqtt/scripts/publishers/unraid-api/system.sh
+++ source/usr/local/emhttp/plugins/unraid-stats2mqtt/scripts/publishers/unraid-api/system.sh
@@ -5,7 +5,12 @@ publish_uptime() {
   local info; info=$(get_info_data) || return
   local uptime_seconds; uptime_seconds=$(echo "$info" | jq -r '.data.info.os.uptime // empty')
   [ -z "$uptime_seconds" ] && return
+  local days=$(( uptime_seconds / 86400 ))
+  local hours=$(( (uptime_seconds % 86400) / 3600 ))
+  local minutes=$(( (uptime_seconds % 3600) / 60 ))
+  local secs=$(( uptime_seconds % 60 ))
+  local uptime_fmt; printf -v uptime_fmt "%d %02d:%02d:%02d" "$days" "$hours" "$minutes" "$secs"
   local base="${MQTT_BASE_TOPIC}/sensor/${MQTT_TOPIC}"
   ha_register "system_uptime" "System Uptime" "${base}_system_uptime/state" "s" "duration" "timer-outline" "" "$expire"
-  mqtt_publish "${base}_system_uptime/state" "$uptime_seconds" "$retain"
+  mqtt_publish "${base}_system_uptime/state" "$uptime_fmt" "$retain"
 }
